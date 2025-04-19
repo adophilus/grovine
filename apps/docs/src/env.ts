7d8b47dd618fc261e91af8e9c5de8cfa9a8e1fbf
Adophilus <uchenna19of@gmail.com>
2025-04-19 14:26:16 +0100

    feat: added nx

--- apps/docs/src/env.ts
+++ apps/docs/src/env.ts
@@ -6,12 +6,12 @@ const ENV = import.meta.env;
 export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
-    NODE_ENV: z.enum(["production", "staging", "development", "test"]),
-    BACKEND_URL: z.string().url(),
+    VITE_NODE_ENV: z.enum(["production", "staging", "development", "test"]),
+    VITE_BACKEND_URL: z.string().url(),
   },
   server: {},
   runtimeEnv: {
-    NODE_ENV: ENV.VITE_NODE_ENV,
-    BACKEND_URL: ENV.BACKEND_URL,
+    VITE_NODE_ENV: ENV.VITE_NODE_ENV,
+    VITE_BACKEND_URL: ENV.BACKEND_URL,
   },
 });
