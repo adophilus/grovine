ef5eea0281640b6398b93a5647c68e2c434439cd
Adophilus <uchenna19of@gmail.com>
2025-06-06 23:16:29 +0100

    chore: added the env script import to the build config script of the docs

--- apps/docs/src/env.ts
+++ apps/docs/src/env.ts
@@ -1,17 +1,17 @@
-import { createEnv } from "@t3-oss/env-core";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-core'
+import { z } from 'zod'
 
-const ENV = import.meta.env;
+const ENV = Object.assign({}, process.env, import.meta.env)
 
 export const env = createEnv({
-  clientPrefix: "VITE_",
+  clientPrefix: 'VITE_',
   client: {
-    VITE_NODE_ENV: z.enum(["production", "staging", "development", "test"]),
-    VITE_BACKEND_URL: z.string().url(),
+    VITE_NODE_ENV: z.enum(['production', 'staging', 'development', 'test']),
+    VITE_BACKEND_URL: z.string().url()
   },
   server: {},
   runtimeEnv: {
     VITE_NODE_ENV: ENV.VITE_NODE_ENV,
-    VITE_BACKEND_URL: ENV.VITE_BACKEND_URL,
-  },
-});
+    VITE_BACKEND_URL: ENV.VITE_BACKEND_URL
+  }
+})
