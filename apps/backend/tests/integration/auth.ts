5950acdaabef3243ba4130d4a213fd6abb02efd0
Gospel <chibuisiukegbu@gmail.com>
2025-06-07 18:28:04 +0100

    chore: began wotkin on the auth signin tests

--- apps/backend/tests/integration/auth.ts
+++ apps/backend/tests/integration/auth.ts
@@ -10,6 +10,7 @@ const client = createClient(config.server.url);
 
 describe("auth", () => {
   const email = faker.internet.email();
+  console.log("email", email); 
 
   test("sign up", async () => {
     const res = await client.POST("/auth/sign-up", {
@@ -50,4 +51,69 @@ describe("auth", () => {
 
     assert(!res.error, res.error?.code);
   });
+
+  
+  test("sign in", async () => {
+    const findUserResult = await AuthRepository.findUserByEmail(email);
+    assert(findUserResult.isOk, "User should be created");
+    assert(findUserResult.value, "User should not be null");
+
+    const findOtpResult = await AuthRepository.findTokenByUserIdAndPurpose({
+      user_id: findUserResult.value.id,
+      purpose: "SIGN_IN_OTP",
+    });
+
+    assert(findOtpResult.isOk, "OTP should be created");
+    assert(findOtpResult.value, "OTP should not be null");
+
+    const otp = findOtpResult.value;
+
+
+    const res = await client.POST("/auth/sign-in",{
+      body: {
+        email,
+        otp,
+      },
+    })
+  })
+  test("sign in verification", async () => {
+
+
+    const findUserResult = await AuthRepository.findUserByEmail(email);
+    assert(findUserResult.isOk, "User should be created");
+    assert(findUserResult.value, "User should not be null");
+
+    const user = findUserResult.value;
+
+    const findTokenResult = await AuthRepository.findTokenByUserIdAndPurpose({
+      user_id: user.id,
+      purpose: "SIGN_IN_VERIFICATION",
+    });
+
+    assert(findTokenResult.isOk, "Token should be created");
+    assert(findTokenResult.value, "Token should not be null");
+
+    const token = findTokenResult.value;
+
+    const res = await client.POST("/auth/sign-in/verification", {
+      body: {
+        email,
+        otp: token.token,
+      },
+    });
+
+    assert(!res.error, res.error?.code);
+
+  })
+  test("sign in verification resend", async () => {
+    const res = await client.POST("/auth/sign-in/verification/resend", {
+      body: {
+        email,
+      },
+    });
+
+    assert(!res.error, res.error?.code);
+  });
+
 });
+
