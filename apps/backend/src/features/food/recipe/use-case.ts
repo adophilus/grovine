4fbf8bc4e34ab0bd586920ea34b102831e48e207
Adophilus <uchenna19of@gmail.com>
2025-07-30 21:42:24 +0100

    feat: added use-case re-export for food recipes

---
+++ apps/backend/src/features/food/recipe/use-case.ts
@@ -0,0 +1,13 @@
+import CreateRecipeUseCase from './route/create/use-case'
+import GetRecipeUseCase from './route/get/use-case'
+import ListRecipesUseCase from './route/list/use-case'
+import UpdateRecipeUseCase from './route/update/use-case'
+import DeleteRecipeUseCase from './route/delete/use-case'
+
+export {
+  CreateRecipeUseCase,
+  GetRecipeUseCase,
+  ListRecipesUseCase,
+  UpdateRecipeUseCase,
+  DeleteRecipeUseCase
+}
